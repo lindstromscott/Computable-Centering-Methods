1a4edfe7224c2730502646e9eb1582f30143319c
lindstromscott <lindstrom.scott@gmail.com>
2020-02-26 12:51:29 +0800

    Add files via upload

--- Matlab Code/basis_pursuit_LT_smart.m
+++ Matlab Code/basis_pursuit_LT_smart.m
@@ -1,4 +1,4 @@
-function [z, history] = basis_pursuit_LT_smart(A, b, rho, alpha)
+function [z, history] = basis_pursuit_LT_smart_fast(A, b, rho, alpha)
 % basis_pursuit  Solve basis pursuit via ADMM
 %
 % [x, history] = basis_pursuit(A, b, rho, alpha)
@@ -28,7 +28,7 @@ t_start = tic;
 %% Global constants and defaults
 
 QUIET    = 0;
-MAX_ITER = 50000;
+MAX_ITER = 1000000;
 ABSTOL   = 1e-8;
 RELTOL   = 1e-8;
 
@@ -57,80 +57,38 @@ AAt = A*A';
 P = eye(n) - A' * (AAt \ A);
 q = A' * (AAt \ b);
 
-%We start with regular ADMM
-
-for k = 1:2
-    
-    % x-update
-    x = P*(z - u) + q;
-
-    % z-update with relaxation
-    zold = z;
-    x_hat = alpha*x + (1 - alpha)*zold;
-    z = shrinkage(x_hat + u, 1/rho);
-
-    uold = u;
-    u = u + (x_hat - z);
-    
-    %I save the previous DR iterate
-    Rachold = Rach;
-    %I compute the next DR iterate
-    Rach = u + rho*z; 
-    
-    
-
-    % diagnostics, reporting, termination checks
-    history.objval(k)  = objective(A, b, x);
-    
-    history.u_diff(k)  = norm(uold-u);
-
-    history.r_norm(k)  = norm(x - z);
-    history.s_norm(k)  = norm(-rho*(z - zold));
-    
-    %I record the DR iterate subsequent differences
-    history.Rach_diff(k) = norm(Rachold-Rach);
-    
-    history.eps_pri(k) = sqrt(n)*ABSTOL + RELTOL*max(norm(x), norm(-z));
-    history.eps_dual(k)= sqrt(n)*ABSTOL + RELTOL*norm(rho*u);
-    history.eps_u(k)= sqrt(n)*ABSTOL + RELTOL*norm(u);
-
-    if ~QUIET
-        fprintf('%3d\t%10.4f\t%10.4f\t%10.4f\t%10.4f\t%10.2f\n', k, ...
-            history.r_norm(k), history.eps_pri(k), ...
-            history.s_norm(k), history.eps_dual(k), history.objval(k));
-    end
-
-    if (history.r_norm(k) < history.eps_pri(k) && ...
-       history.s_norm(k) < history.eps_dual(k))
-         break;
-    end
-end
 
 %Now we do the accelerated version
 uREG = u;
 uLT = u;
 zLT = z;
+j = -1;
 
 
-for k = 2:MAX_ITER
+for k = 1:MAX_ITER
     
     uold = u;
     
-    if mod(k,3) == 0 %if it's time to check the centering step, enter
+    if j == 2 %if it's time to check the centering step, enter
         % x-update
        xREG = P*(z - uREG) + q;
         xLT = P*(zLT - uLT) + q;
        if objective(A, b, xREG) > objective(A,b,xLT)
             x = xLT;
             u = uLT;
+            j=0; %when we accept the centering step, we set the counter back to zero
             
-       else
+       else %we reject the centered update, in which case we must update indices and leave counter unchanged
            x = xREG;
            u = uREG; 
+           y1 = y2;
+           y2 = y3;
+           %j = 2; counter stays unchanged
            
        end
     else %if it isn't time to check the centering step, skip the objective function check and update normally
         x = P*(z - u) + q;
+        j = j+1;
     end
 
     % z-update with relaxation
@@ -142,16 +100,16 @@ for k = 2:MAX_ITER
 	u = u + (x_hat - z);
     
     %y_{k-1} update
-    if mod(k,3) == 0
+    if j == 0
         y1 = u+rho*z;
-    elseif mod(k,3) == 1
+    elseif j == 1
         y2 = u+rho*z;
-    else
+    else %then j == 2 and so
         y3 = u+rho*z;
     end
     
     %compute the alternative multiplier candidate based on centering
-    if mod(k,3) == 2 && colinear_check(y1,2*y2-y1,piT(y1,y2,y3),10^(-20)) == 1
+    if j == 2 && colinear_check(y1,2*y2-y1,piT(y1,y2,y3),10^(-20)) == 1
 
         %on third updates, I circumcenter the dual if not colinear.
         %I then set shadow u to be prox_cd2 of the dual governing sequence
@@ -165,6 +123,8 @@ for k = 2:MAX_ITER
     end
     uREG = u;
     zREG = z;
+    
+
     
     
 
